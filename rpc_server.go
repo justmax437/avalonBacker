eefff9b4fca3dc7175b10985b8c820b5ea31b6b1
Maxim Vasilenko <renxi@yandex.ru>
2020-10-28 20:45:31 +0300

    Add entry point, start default gRPC

---
+++ rpc_server.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"github.com/justmax437/avalonBacker/api"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+	"os"
+	"time"
+)
+
+func main() {
+	grpcServer := grpc.NewServer()
+	api.RegisterGameServiceServer(grpcServer,
+		NewGameService(
+			NewMemoryStorage(30*time.Minute),
+		),
+	)
+
+	err := grpcServer.Serve(getServerSocket())
+	if err != nil {
+		log.Fatal("failed to start gRPC server: ", err)
+	}
+}
+
+func getServerSocket() net.Listener {
+	var sock net.Listener
+	if port, exist := os.LookupEnv("PORT"); exist {
+		socket, err := net.Listen("tcp", ":"+port)
+		if err != nil {
+			log.Fatal("failed to open socket - ", err)
+		}
+		sock = socket
+	} else {
+		log.Fatal("server port number env was not found (check $PORT)")
+	}
+	return sock
+}
