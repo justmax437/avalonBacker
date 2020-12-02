b29c4b537886c5a77d34a1083329f2abbb642c80
Maxim Vasilenko <renxi@yandex.ru>
2020-12-02 16:57:41 +0300

    Fix team votes #2

--- votes_storage.go
+++ votes_storage.go
@@ -9,66 +9,66 @@ import (
 type VoteStorage struct {
 	missionVotes         map[uuid.UUID]int8
 	teamVotes            map[uuid.UUID]int8
-	playersVotedMissions map[uint64]bool
-	playersVotedTeams    map[uint64]bool
+	playersVotedMissions map[uuid.UUID]map[uint64]bool
+	playersVotedTeams    map[uuid.UUID]map[uint64]bool
 }
 
 func NewVoteStorage() *VoteStorage {
 	return &VoteStorage{
 		make(map[uuid.UUID]int8, 0),
 		make(map[uuid.UUID]int8, 0),
-		make(map[uint64]bool),
-		make(map[uint64]bool),
+		make(map[uuid.UUID]map[uint64]bool),
+		make(map[uuid.UUID]map[uint64]bool),
 	}
 }
 
-func (v *VoteStorage) AddPositiveMissionVote(id uuid.UUID, player *api.Player) {
-	if _, alreadyVoted := v.playersVotedMissions[player.Id]; alreadyVoted {
-		log.Println("repeated vote attempt by", player)
+func (v *VoteStorage) AddPositiveMissionVote(gameId uuid.UUID, player *api.Player) {
+	if _, alreadyVoted := v.playersVotedMissions[gameId][player.Id]; alreadyVoted {
+		log.Println(gameId, "repeated vote attempt by", player)
 		return
 	}
-	v.missionVotes[id]++
-	v.playersVotedMissions[player.Id] = true
+	v.missionVotes[gameId]++
+	v.playersVotedMissions[gameId][player.Id] = true
 }
 
-func (v *VoteStorage) AddNegativeMissionVote(id uuid.UUID, player *api.Player) {
-	if _, alreadyVoted := v.playersVotedMissions[player.Id]; alreadyVoted {
-		log.Println("repeated vote attempt by", player)
+func (v *VoteStorage) AddNegativeMissionVote(gameId uuid.UUID, player *api.Player) {
+	if _, alreadyVoted := v.playersVotedMissions[gameId][player.Id]; alreadyVoted {
+		log.Println(gameId, "repeated vote attempt by", player)
 		return
 	}
-	v.missionVotes[id]--
-	v.playersVotedMissions[player.Id] = true
+	v.missionVotes[gameId]--
+	v.playersVotedMissions[gameId][player.Id] = true
 }
 
 func (v *VoteStorage) GetMissionVotesCountForGame(id uuid.UUID) int8 {
 	return v.missionVotes[id]
 }
 
-func (v *VoteStorage) AddPositiveTeamVote(id uuid.UUID, player *api.Player) {
-	if _, alreadyVoted := v.playersVotedMissions[player.Id]; alreadyVoted {
-		log.Println("repeated vote attempt by", player)
+func (v *VoteStorage) AddPositiveTeamVote(gameId uuid.UUID, player *api.Player) {
+	if _, alreadyVoted := v.playersVotedMissions[gameId][player.Id]; alreadyVoted {
+		log.Println(gameId, "repeated vote attempt by", player)
 		return
 	}
-	v.teamVotes[id]++
-	v.playersVotedTeams[player.Id] = true
+	v.teamVotes[gameId]++
+	v.playersVotedTeams[gameId][player.Id] = true
 }
 
-func (v *VoteStorage) AddNegativeTeamVote(id uuid.UUID, player *api.Player) {
-	if _, alreadyVoted := v.playersVotedTeams[player.Id]; alreadyVoted {
-		log.Println("repeated vote attempt by", player)
+func (v *VoteStorage) AddNegativeTeamVote(gameId uuid.UUID, player *api.Player) {
+	if _, alreadyVoted := v.playersVotedTeams[gameId][player.Id]; alreadyVoted {
+		log.Println(gameId, "repeated vote attempt by", player)
 		return
 	}
-	v.teamVotes[id]--
-	v.playersVotedTeams[player.Id] = true
+	v.teamVotes[gameId]--
+	v.playersVotedTeams[gameId][player.Id] = true
 }
 
 func (v *VoteStorage) GetTeamVotesCountForGame(id uuid.UUID) int8 {
 	return v.teamVotes[id]
 }
 
-func (v *VoteStorage) ResetVotes() {
-	v.missionVotes = make(map[uuid.UUID]int8, 0)
-	v.missionVotes = make(map[uuid.UUID]int8, 0)
-	v.playersVotedMissions = make(map[uint64]bool)
-	v.playersVotedTeams = make(map[uint64]bool)
+func (v *VoteStorage) ResetVotes(gameId uuid.UUID) {
+	v.missionVotes[gameId] = 0
+	v.missionVotes[gameId] = 0
+	v.playersVotedMissions[gameId] = make(map[uint64]bool)
+	v.playersVotedTeams[gameId] = make(map[uint64]bool)
 }
