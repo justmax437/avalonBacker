b39c8b5e6900a1f51affd6a319e282031eff61e7
Maxim Vasilenko <renxi@yandex.ru>
2020-12-02 16:51:11 +0300

    Teams votes support

--- votes_storage.go
+++ votes_storage.go
@@ -7,35 +7,68 @@ import (
 )
 
 type VoteStorage struct {
-	missionVotes map[uuid.UUID]int8
-	playersVoted map[uint64]bool
+	missionVotes         map[uuid.UUID]int8
+	teamVotes            map[uuid.UUID]int8
+	playersVotedMissions map[uint64]bool
+	playersVotedTeams    map[uint64]bool
 }
 
 func NewVoteStorage() *VoteStorage {
 	return &VoteStorage{
 		make(map[uuid.UUID]int8, 0),
+		make(map[uuid.UUID]int8, 0),
+		make(map[uint64]bool),
 		make(map[uint64]bool),
 	}
 }
 
 func (v *VoteStorage) AddPositiveMissionVote(id uuid.UUID, player *api.Player) {
-	if _, alreadyVoted := v.playersVoted[player.Id]; alreadyVoted {
+	if _, alreadyVoted := v.playersVotedMissions[player.Id]; alreadyVoted {
 		log.Println("repeated vote attempt by", player)
 		return
 	}
 	v.missionVotes[id]++
-	v.playersVoted[player.Id] = true
+	v.playersVotedMissions[player.Id] = true
 }
 
 func (v *VoteStorage) AddNegativeMissionVote(id uuid.UUID, player *api.Player) {
-	if _, alreadyVoted := v.playersVoted[player.Id]; alreadyVoted {
+	if _, alreadyVoted := v.playersVotedMissions[player.Id]; alreadyVoted {
 		log.Println("repeated vote attempt by", player)
 		return
 	}
 	v.missionVotes[id]--
-	v.playersVoted[player.Id] = true
+	v.playersVotedMissions[player.Id] = true
 }
 
 func (v *VoteStorage) GetMissionVotesCountForGame(id uuid.UUID) int8 {
 	return v.missionVotes[id]
+}
+
+func (v *VoteStorage) AddPositiveTeamVote(id uuid.UUID, player *api.Player) {
+	if _, alreadyVoted := v.playersVotedMissions[player.Id]; alreadyVoted {
+		log.Println("repeated vote attempt by", player)
+		return
+	}
+	v.teamVotes[id]++
+	v.playersVotedTeams[player.Id] = true
+}
+
+func (v *VoteStorage) AddNegativeTeamVote(id uuid.UUID, player *api.Player) {
+	if _, alreadyVoted := v.playersVotedTeams[player.Id]; alreadyVoted {
+		log.Println("repeated vote attempt by", player)
+		return
+	}
+	v.teamVotes[id]--
+	v.playersVotedTeams[player.Id] = true
+}
+
+func (v *VoteStorage) GetTeamVotesCountForGame(id uuid.UUID) int8 {
+	return v.teamVotes[id]
+}
+
+func (v *VoteStorage) ResetVotes() {
+	v.missionVotes = make(map[uuid.UUID]int8, 0)
+	v.missionVotes = make(map[uuid.UUID]int8, 0)
+	v.playersVotedMissions = make(map[uint64]bool)
+	v.playersVotedTeams = make(map[uint64]bool)
 }
