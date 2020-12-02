da01315b18a1e03c76b50392e03d0677d864406e
Maxim Vasilenko <renxi@yandex.ru>
2020-12-02 18:33:21 +0300

    More game logic

--- game_service.go
+++ game_service.go
@@ -130,14 +130,25 @@ func (g *simpleGameService) PushGameState(_ context.Context, session *api.GameSe
 		}
 
 		if g.votes.GetTeamVotesCountForGame(apiIDToUUID(session.GameId)) <= 0 {
+			// Mission Failed
 			game.Mission.TimesVoted++
 			if game.Mission.TimesVoted == 5 {
 				game.State = api.GameSession_EVIL_TEAM_WON
 				return nil, nil
 			}
-			//TODO pass leadership
-		} else {
 
+			if game.TotalPlayersCount() > game.CurrentLeaderIndex {
+				game.CurrentLeaderIndex++
+				game.Leader = game.AllPlayers[game.CurrentLeaderIndex]
+			}
+
+			if err := g.sessions.StoreSession(game); err != nil {
+				return nil, errors.New("failed to store session data: " + err.Error())
+			}
+
+			return &game.GameSession, nil
+		} else {
+			//Mission succeeded
 		}
 
 		// TODO defer from push state func
