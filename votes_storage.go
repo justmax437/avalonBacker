6a25c87eac6218872a1f1dd376916fdf531533d6
Maxim Vasilenko <renxi@yandex.ru>
2020-12-02 16:39:22 +0300

    Forbid repeated votes

--- votes_storage.go
+++ votes_storage.go
@@ -1,22 +1,39 @@
 package main
 
-import "github.com/google/uuid"
+import (
+	"github.com/google/uuid"
+	"github.com/justmax437/avalonBacker/api"
+	"log"
+)
 
 type VoteStorage struct {
 	missionVotes map[uuid.UUID]int8
+	playersVoted map[uint64]bool
 }
 
 func NewVoteStorage() *VoteStorage {
-	return &VoteStorage{make(map[uuid.UUID]int8, 0)}
-
+	return &VoteStorage{
+		make(map[uuid.UUID]int8, 0),
+		make(map[uint64]bool),
+	}
 }
 
-func (v *VoteStorage) AddPositiveMissionVote(id uuid.UUID) {
+func (v *VoteStorage) AddPositiveMissionVote(id uuid.UUID, player *api.Player) {
+	if _, alreadyVoted := v.playersVoted[player.Id]; alreadyVoted {
+		log.Println("repeated vote attempt by", player)
+		return
+	}
 	v.missionVotes[id]++
+	v.playersVoted[player.Id] = true
 }
 
-func (v *VoteStorage) AddNegativeMissionVote(id uuid.UUID) {
+func (v *VoteStorage) AddNegativeMissionVote(id uuid.UUID, player *api.Player) {
+	if _, alreadyVoted := v.playersVoted[player.Id]; alreadyVoted {
+		log.Println("repeated vote attempt by", player)
+		return
+	}
 	v.missionVotes[id]--
+	v.playersVoted[player.Id] = true
 }
 
 func (v *VoteStorage) GetMissionVotesCountForGame(id uuid.UUID) int8 {
